404011f7ce53f62c60370901005e89af3173d034
yein <inwon9892@naver.com>
2024-03-28 02:36:49 +0900

    init commit

---
+++ app/src/main/java/kr/co/lion/unipiece/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.lion.unipiece
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
