04c541e38622ce27d418e13321c4bba72ecc4bba
yein <inwon9892@naver.com>
2024-03-28 16:20:18 +0900

    [Feat] #7 BuyFragment Toolbar menu 연결 및 아이콘 각각 추가

--- app/src/main/java/kr/co/lion/unipiece/ui/buy/BuyFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/buy/BuyFragment.kt
@@ -6,55 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.co.lion.unipiece.R
+import kr.co.lion.unipiece.databinding.FragmentBuyBinding
+import kr.co.lion.unipiece.util.setMenuIconColor
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BuyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BuyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentBuyBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buy, container, false)
+
+        binding = FragmentBuyBinding.inflate(inflater, container, false)
+
+        settingToolbarBuy()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BuyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BuyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun settingToolbarBuy(){
+
+        with(binding) {
+            toolbarBuy.apply {
+
+                setNavigationIcon(R.drawable.menu_icon)
+
+                inflateMenu(R.menu.menu_search_cart)
+
+                requireContext().setMenuIconColor(menu, R.id.menu_search, R.color.third)
+                requireContext().setMenuIconColor(menu, R.id.menu_cart, R.color.second)
+
             }
+        }
     }
+
 }
