02d8b32fafad3e53ddd3c573c5f06f93df8d7c45
yein <inwon9892@naver.com>
2024-03-28 11:57:38 +0900

    [Add] #3 bottomnavigationbar에 들어가는 fragment 추가

--- app/src/main/java/kr/co/lion/unipiece/ui/MainActivity.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.co.lion.unipiece
+package kr.co.lion.unipiece.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.lion.unipiece.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
