022954e482bd5c07718b2f3e2ad3f195973c25b7
seongukHeo <heoseonguk3@gmail.com>
2024-04-01 14:41:11 +0900

    [Feat]#23_InfoAllActivity 구현 완료

---
+++ app/src/main/java/kr/co/lion/unipiece/ui/infomation/InfoAllActivity.kt
@@ -0,0 +1,90 @@
+package kr.co.lion.unipiece.ui.infomation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import kr.co.lion.unipiece.R
+import kr.co.lion.unipiece.databinding.ActivityInfoAllBinding
+import kr.co.lion.unipiece.databinding.InfoAllBinding
+
+class InfoAllActivity : AppCompatActivity() {
+
+    lateinit var activityInfoAllBinding: ActivityInfoAllBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityInfoAllBinding = ActivityInfoAllBinding.inflate(layoutInflater)
+        setContentView(activityInfoAllBinding.root)
+        settingToolBar()
+        settingAdapter()
+    }
+
+    //툴바
+    private fun settingToolBar(){
+        activityInfoAllBinding.apply {
+            toolBarInfoAll.apply {
+                title = "소식 전체 보기"
+                setNavigationIcon(R.drawable.back_icon)
+                setNavigationOnClickListener {
+                    finish()
+                }
+                inflateMenu(R.menu.menu_home)
+                setOnMenuItemClickListener {
+                    when(it.itemId){
+                        R.id.menu_home -> {
+                            finish()
+                        }
+                    }
+
+                    true
+                }
+            }
+        }
+    }
+
+
+    private fun settingAdapter(){
+        activityInfoAllBinding.apply {
+            recyclerviewInfoAll.apply {
+                adapter = RecyclerviewAdapter()
+                layoutManager = LinearLayoutManager(this@InfoAllActivity)
+            }
+        }
+    }
+
+
+    inner class RecyclerviewAdapter:RecyclerView.Adapter<RecyclerviewAdapter.ViewHolderClass>(){
+
+        inner class ViewHolderClass(infoAllBinding: InfoAllBinding):RecyclerView.ViewHolder(infoAllBinding.root){
+            var infoAllBinding :InfoAllBinding
+
+            init {
+                this.infoAllBinding = infoAllBinding
+                this.infoAllBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+            var infoAllBinding = InfoAllBinding.inflate(layoutInflater)
+            var viewHolder = ViewHolderClass(infoAllBinding)
+            return viewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+            holder.infoAllBinding.imageView2.setImageResource(R.drawable.icon)
+            holder.infoAllBinding.textInfoAllTitle.text = "UniPiece"
+            holder.infoAllBinding.textInfoAllDate.text = "2024-04-01 ~ 2024-04-26"
+            holder.infoAllBinding.textInfoAllAuthorName.text = "멋쟁이 사람들"
+        }
+    }
+}
