bcb7eec40896129d46005c2a21225cf4b40fafaf
rmadmsjm <dmswjdals12@naver.com>
2024-04-01 17:54:08 +0900

    [Feat] #25 작품 카테고리 Dialog

--- app/src/main/java/kr/co/lion/unipiece/ui/mygallery/SalesApplicationActivity.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mygallery/SalesApplicationActivity.kt
@@ -1,12 +1,18 @@
 package kr.co.lion.unipiece.ui.mygallery
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.core.view.isVisible
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kr.co.lion.unipiece.R
 import kr.co.lion.unipiece.databinding.ActivitySalesApplicationBinding
+import kr.co.lion.unipiece.databinding.CategoryDialogBinding
 import kr.co.lion.unipiece.util.ToolUtil
 
 class SalesApplicationActivity : AppCompatActivity() {
@@ -46,6 +52,7 @@ class SalesApplicationActivity : AppCompatActivity() {
             textFieldSalesApplicationPieceName.apply {
                 setOnEditorActionListener { v, actionId, event ->
                     ToolUtil.hideSoftInput(this@SalesApplicationActivity)
+                    showCategoryDialog()
                     true
                 }
             }
@@ -82,5 +89,51 @@ class SalesApplicationActivity : AppCompatActivity() {
         }
 
         datePicker.show(supportFragmentManager, datePicker.toString())
+    }
+
+    fun showCategoryDialog() {
+        MaterialAlertDialogBuilder(this@SalesApplicationActivity).apply {
+            setTitle("카테고리")
+
+            val categoryDialogBinding = CategoryDialogBinding.inflate(layoutInflater)
+            setView(categoryDialogBinding.root)
+
+            categoryDialogBinding.apply {
+                radioGroup1.isVisible = false
+                radioGroup2.isVisible = false
+                radioGroup3.isVisible = false
+
+                cardViewCategoryDialogArt.setOnClickListener {
+                    toggleRadioGroupVisibility(radioGroup1, imageViewCategoryDialogArt, !radioGroup1.isVisible)
+                }
+
+                cardViewCategoryDialogHumanities.setOnClickListener {
+                    toggleRadioGroupVisibility(radioGroup2, imageViewCategoryDialogHumanities, !radioGroup2.isVisible)
+                }
+
+                cardViewCategoryDialogEngineering.setOnClickListener {
+                    toggleRadioGroupVisibility(radioGroup3, imageViewCategoryDialogEngineering, !radioGroup3.isVisible)
+                }
+            }
+
+            setNegativeButton("취소", null)
+            setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                val selectedRadioButton = when {
+                    categoryDialogBinding.radioGroup1.checkedRadioButtonId != -1 -> categoryDialogBinding.radioGroup1.findViewById<RadioButton>(categoryDialogBinding.radioGroup1.checkedRadioButtonId)
+                    categoryDialogBinding.radioGroup2.checkedRadioButtonId != -1 -> categoryDialogBinding.radioGroup2.findViewById<RadioButton>(categoryDialogBinding.radioGroup2.checkedRadioButtonId)
+                    else -> categoryDialogBinding.radioGroup3.findViewById<RadioButton>(categoryDialogBinding.radioGroup3.checkedRadioButtonId)
+                }
+                binding.textFieldSalesApplicationCategory.setText(selectedRadioButton?.text ?: "")
+
+                ToolUtil.showSoftInput(this@SalesApplicationActivity, binding.textFieldSalesApplicationPrice)
+            }
+
+            show()
+        }
+    }
+
+    fun toggleRadioGroupVisibility(radioGroup: RadioGroup, imageView: ImageView, show: Boolean) {
+        radioGroup.isVisible = !radioGroup.isVisible
+        imageView.setImageResource(if (show) R.drawable.arrowdropup_icon else R.drawable.arrowdropdown_icon)
     }
 }
