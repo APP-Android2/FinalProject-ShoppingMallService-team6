3431de9f118dc5afb5291b3c2ed4fbf6b094c7ba
rmadmsjm <dmswjdals12@naver.com>
2024-04-01 10:32:19 +0900

    [Feat] #11 주문 취소 및 반품 접수 버튼 리스너

--- app/src/main/java/kr/co/lion/unipiece/ui/mygallery/PurchasedPieceDetailFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mygallery/PurchasedPieceDetailFragment.kt
@@ -8,18 +8,22 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import kr.co.lion.unipiece.R
 import kr.co.lion.unipiece.databinding.FragmentPurchasedPieceDetailBinding
+import kr.co.lion.unipiece.util.PurchasedPieceDetailFragmentName
 import kr.co.lion.unipiece.util.setMenuIconColor
 
 class PurchasedPieceDetailFragment : Fragment() {
 
     lateinit var binding: FragmentPurchasedPieceDetailBinding
+    lateinit var PurchasedPieceDetailActivity: PurchasedPieceDetailActivity
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding = FragmentPurchasedPieceDetailBinding.inflate(inflater, container, false)
+        PurchasedPieceDetailActivity = activity as PurchasedPieceDetailActivity
 
         settingToolbar()
         settingRefundApprovalView()
+        settingButtons()
 
         return binding.root
     }
@@ -42,6 +46,18 @@ class PurchasedPieceDetailFragment : Fragment() {
         binding.apply {
             linearLayoutRefundApproval.isVisible = false
             dividerRefundApproval.isVisible = false
+        }
+    }
+
+    fun settingButtons() {
+        binding.apply {
+            buttonPurchasedPieceDetailOrderCancel.setOnClickListener {
+                PurchasedPieceDetailActivity.replaceFragment(PurchasedPieceDetailFragmentName.PURCHASE_CANCEL_FRAGEMNT, true)
+            }
+
+            buttonPurchasedPieceDetailRefund.setOnClickListener {
+                PurchasedPieceDetailActivity.replaceFragment(PurchasedPieceDetailFragmentName.REFUND_FRAGMENT, true)
+            }
         }
     }
 }
