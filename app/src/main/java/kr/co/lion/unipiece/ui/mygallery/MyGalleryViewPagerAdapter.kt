0fe8bf2ef05e00dcc0c25361c2d8a4df6c24dae4
rmadmsjm <dmswjdals12@naver.com>
2024-04-02 17:45:09 +0900

    [Feat] #32 나의 갤러리 viewPager 수정

--- app/src/main/java/kr/co/lion/unipiece/ui/mygallery/MyGalleryViewPagerAdapter.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mygallery/MyGalleryViewPagerAdapter.kt
@@ -2,12 +2,15 @@ package kr.co.lion.unipiece.ui.mygallery
 
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class MyGalleryViewPagerAdapter(
-    private val fragmentList: ArrayList<Fragment>,
-    container: AppCompatActivity)
-    : FragmentStateAdapter(container.supportFragmentManager, container.lifecycle) {
-    override fun getItemCount(): Int = fragmentList.count()
+    private val fragmentList: List<Fragment>,
+    container: FragmentActivity
+) : FragmentStateAdapter(container) {
+
+    override fun getItemCount(): Int = fragmentList.size
+
     override fun createFragment(position: Int): Fragment = fragmentList[position]
 }
