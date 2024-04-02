ba12345f13ad909496d332cd0de68d92529ad5b1
ums1212 <voll1212@gmail.com>
2024-04-02 13:30:08 +0900

    [Feat] #31 액티비티 및 프래그먼트 생성

---
+++ app/src/main/java/kr/co/lion/unipiece/ui/author/AuthorInfoActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.lion.unipiece.ui.author
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.lion.unipiece.R
+
+class AuthorInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_author_info)
+    }
+}
---
+++ app/src/main/java/kr/co/lion/unipiece/ui/author/AuthorReviewBottomSheetFragment.kt
@@ -0,0 +1,20 @@
+package kr.co.lion.unipiece.ui.author
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.co.lion.unipiece.R
+
+class AuthorReviewBottomSheetFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_author_review_bottom_sheet, container, false)
+    }
+
+}
