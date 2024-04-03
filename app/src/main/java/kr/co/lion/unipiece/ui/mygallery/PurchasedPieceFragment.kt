ebf5d9d5fddc3b040d1048ca8092a93173f03ec5
rmadmsjm <dmswjdals12@naver.com>
2024-04-03 14:14:41 +0900

    [Feat] #32 구매한 작품 fragment

--- app/src/main/java/kr/co/lion/unipiece/ui/mygallery/PurchasedPieceFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mygallery/PurchasedPieceFragment.kt
@@ -1,60 +1,76 @@
 package kr.co.lion.unipiece.ui.mygallery
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import kr.co.lion.unipiece.R
+import kr.co.lion.unipiece.databinding.FragmentPurchasedPieceBinding
+import kr.co.lion.unipiece.databinding.RowPurchasedPieceBinding
+import kr.co.lion.unipiece.ui.MainActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PurchasedPieceFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PurchasedPieceFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    lateinit var binding: FragmentPurchasedPieceBinding
+    lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentPurchasedPieceBinding.inflate(inflater, container, false)
+        mainActivity = activity as MainActivity
+
+        settingRecyclerView()
+
+        return binding.root
+    }
+
+    fun settingRecyclerView() {
+        binding.apply {
+            recyclerViewPurchasedPiece.apply {
+                adapter = RecyclerViewAdpater()
+                layoutManager = LinearLayoutManager(mainActivity)
+                val deco = MaterialDividerItemDecoration(mainActivity, MaterialDividerItemDecoration.VERTICAL)
+                deco.dividerInsetStart = 50
+                deco.dividerInsetEnd = 50
+                deco.dividerColor = ContextCompat.getColor(mainActivity, R.color.lightgray)
+                addItemDecoration(deco)
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_purchased_piece, container, false)
+    inner class RecyclerViewAdpater : RecyclerView.Adapter<RecyclerViewAdpater.ViewHolder>() {
+        inner class ViewHolder(rowPurchasedPieceBinding: RowPurchasedPieceBinding) : RecyclerView.ViewHolder(rowPurchasedPieceBinding.root) {
+            val rowPurchasedPieceBinding: RowPurchasedPieceBinding
+
+            init {
+                this.rowPurchasedPieceBinding = rowPurchasedPieceBinding
+
+                this.rowPurchasedPieceBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val rowPurchasedPieceBinding = RowPurchasedPieceBinding.inflate(layoutInflater)
+            val viewHolder = ViewHolder(rowPurchasedPieceBinding)
+
+            return viewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.rowPurchasedPieceBinding.textViewRowPurchasedPieceName.text = "$position"
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PurchasedPieceFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PurchasedPieceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
