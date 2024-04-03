a316367fddb14e41629ab03caa4bd808a2e94373
yein <inwon9892@naver.com>
2024-04-04 04:26:22 +0900

    [Feat] #50 검색완료화면 toolbar, searchbar 구현

--- app/src/main/java/kr/co/lion/unipiece/ui/search/SearchResultFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/search/SearchResultFragment.kt
@@ -6,55 +6,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.co.lion.unipiece.R
+import kr.co.lion.unipiece.databinding.FragmentSearchResultBinding
+import kr.co.lion.unipiece.util.setMenuIconColor
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchResultFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchResultFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentSearchResultBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_result, container, false)
+
+        binding = FragmentSearchResultBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchResultFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchResultFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        settingToolbarSearchResult()
+    }
+
+    fun settingToolbarSearchResult(){
+        with(binding.toolbarSearchResult) {
+            setNavigationOnClickListener {
+                activity?.supportFragmentManager?.popBackStack()
+            }
+
+            inflateMenu(R.menu.menu_cart)
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.menu_cart -> {
+
+                        true
+                    }
+                    else -> false
                 }
             }
+            requireContext().setMenuIconColor(menu, R.id.menu_cart, R.color.second)
+        }
     }
 }
