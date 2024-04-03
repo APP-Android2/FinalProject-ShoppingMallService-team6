3dfb77b1ebba2c286079e9dfaa7d4ed3e65c2917
ums1212 <voll1212@gmail.com>
2024-04-03 17:58:12 +0900

    [Feat] #43 전시실 방문 신청 UI 관련 기능

--- app/src/main/java/kr/co/lion/unipiece/ui/mypage/ApplyVisitGalleryFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mypage/ApplyVisitGalleryFragment.kt
@@ -1,20 +1,57 @@
 package kr.co.lion.unipiece.ui.mypage
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.V
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.co.lion.unipiece.R
+import kr.co.lion.unipiece.databinding.ActivityVisitGalleryBinding
+import kr.co.lion.unipiece.databinding.FragmentApplyVisitGalleryBinding
+import kr.co.lion.unipiece.util.VisitGalleryFragmentName
 
 class ApplyVisitGalleryFragment : Fragment() {
+
+    lateinit var fragmentApplyVisitGalleryBinding: FragmentApplyVisitGalleryBinding
+    lateinit var visitGalleryActivity: VisitGalleryActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_apply_visit_gallery, container, false)
+        fragmentApplyVisitGalleryBinding = FragmentApplyVisitGalleryBinding.inflate(inflater)
+        visitGalleryActivity = activity as VisitGalleryActivity
+
+        settingToolbar()
+        settingButtonApplyConfirm()
+
+        return fragmentApplyVisitGalleryBinding.root
+    }
+
+    // 툴바 셋팅
+    private fun settingToolbar(){
+        fragmentApplyVisitGalleryBinding.apply {
+            toolbarApplyVisitGallery.apply {
+                title = "전시실 방문 신청"
+
+                setNavigationIcon(R.drawable.back_icon)
+                setNavigationOnClickListener {
+                    visitGalleryActivity.removeFragment(VisitGalleryFragmentName.APPLY_VISIT_GALLERY_FRAGMENT)
+                }
+            }
+        }
+    }
+
+    // 신청 완료 버튼
+    private fun settingButtonApplyConfirm(){
+        fragmentApplyVisitGalleryBinding.buttonApplyVisitGalleryConfirm.apply {
+            setOnClickListener {
+                // 추후 수정 필요
+                visitGalleryActivity.removeFragment(VisitGalleryFragmentName.APPLY_VISIT_GALLERY_FRAGMENT)
+            }
+        }
     }
 
 }
