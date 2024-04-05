44ddaf40106d274b54b1ca50ab83f2cfb7fa025a
yein <inwon9892@naver.com>
2024-04-05 16:11:11 +0900

    [Feat] #61 작품 랭킹 화면 recyclerview 구현

--- app/src/main/java/kr/co/lion/unipiece/ui/rank/RankPieceFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/rank/RankPieceFragment.kt
@@ -1,60 +1,51 @@
 package kr.co.lion.unipiece.ui.rank
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import kr.co.lion.unipiece.R
+import kr.co.lion.unipiece.databinding.FragmentRankPieceBinding
+import kr.co.lion.unipiece.ui.buy.BuyDetailActivity
+import kr.co.lion.unipiece.ui.rank.adapter.RankPieceAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RankPieceFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RankPieceFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var binding: FragmentRankPieceBinding
+
+    val testPieceList = arrayListOf(R.drawable.logo, R.drawable.icon, R.drawable.test_piece_img,
+        R.drawable.icon, R.drawable.test_piece_img, R.drawable.icon,
+        R.drawable.logo, R.drawable.test_piece_img, R.drawable.logo)
+
+    val adapter: RankPieceAdapter by lazy {
+        RankPieceAdapter(testPieceList,
+            itemClickListener = { testId ->
+                startActivity(Intent(requireActivity(), BuyDetailActivity::class.java))
+            })
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank_piece, container, false)
+
+        binding = FragmentRankPieceBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RankPieceFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RankPieceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView()
+    }
+
+    fun setRecyclerView() {
+        with (binding){
+            rankPieceRV.adapter = adapter
+            rankPieceRV.layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
---
+++ app/src/main/java/kr/co/lion/unipiece/ui/rank/adapter/RankPieceAdapter.kt
@@ -0,0 +1,38 @@
+package kr.co.lion.unipiece.ui.rank.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.lion.unipiece.databinding.ItemRankPieceBinding
+
+class RankPieceAdapter (val imageList: ArrayList<Int>, private val itemClickListener: (Int) -> Unit) : RecyclerView.Adapter<RankPieceViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType:Int): RankPieceViewHolder {
+        val binding: ItemRankPieceBinding = ItemRankPieceBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return RankPieceViewHolder(binding, itemClickListener)
+    }
+
+    override fun onBindViewHolder(holder: RankPieceViewHolder, position: Int){
+        holder.pieceImg.setImageResource(imageList[position])
+    }
+
+    override fun getItemCount(): Int = imageList.size
+}
+
+class RankPieceViewHolder(val binding: ItemRankPieceBinding, private val itemClickListener: (Int) -> Unit): RecyclerView.ViewHolder(binding.root) {
+    val pieceImg: ImageView = binding.pieceImg
+    val pieceRank: TextView = binding.pieceRank
+    val authorName: TextView = binding.authorName
+    val pieceLike: TextView = binding.pieceLike
+    val pieceName: TextView = binding.pieceName
+    val piecePrice: TextView = binding.piecePrice
+    init {
+        binding.root.setOnClickListener {
+            itemClickListener.invoke(position)
+        }
+    }
+}
