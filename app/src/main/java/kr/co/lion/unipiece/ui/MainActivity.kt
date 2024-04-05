8ffe827781de6d7d4fb4fc12fad4f521de8b4f56
yein <inwon9892@naver.com>
2024-04-05 15:22:47 +0900

    [Fix] #61 랭킹에서 안드로이드 뒤로가기 버튼 클릭시 바로 종료되는 오류 수정

--- app/src/main/java/kr/co/lion/unipiece/ui/MainActivity.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
 
     fun initView() {
-        replaceFragment(HOME_FRAGMENT, false)
+        replaceFragment(HOME_FRAGMENT, true)
     }
 
     fun setBuyNaviDrawer(){
