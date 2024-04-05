ec1dab5f84a971674bba91d32ef84c649fdcc165
yein <inwon9892@naver.com>
2024-04-05 17:58:00 +0900

    [Feat] #72 작가 팔로워 순위 화면 구현 완료

--- app/src/main/java/kr/co/lion/unipiece/ui/rank/RankFollowerFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/rank/RankFollowerFragment.kt
@@ -1,60 +1,53 @@
 package kr.co.lion.unipiece.ui.rank
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import kr.co.lion.unipiece.R
+import kr.co.lion.unipiece.databinding.FragmentRankFollowerBinding
+import kr.co.lion.unipiece.ui.buy.BuyDetailActivity
+import kr.co.lion.unipiece.ui.rank.adapter.RankFollowerAdapter
+import kr.co.lion.unipiece.ui.rank.adapter.RankPieceAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RankFollowerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RankFollowerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var binding: FragmentRankFollowerBinding
+
+    val testPieceList = arrayListOf(R.drawable.mypage_icon, R.drawable.icon, R.drawable.mypage_icon,
+        R.drawable.mypage_icon, R.drawable.test_piece_img, R.drawable.icon,
+        R.drawable.logo, R.drawable.test_piece_img, R.drawable.mypage_icon)
+
+    val adapter: RankFollowerAdapter by lazy {
+        RankFollowerAdapter(testPieceList,
+            itemClickListener = { testId ->
+                Log.d("test", testId.toString())
+            })
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank_follower, container, false)
+
+        binding = FragmentRankFollowerBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RankFollowerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RankFollowerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        with (binding){
+            rankFollowerRV.adapter = adapter
+            rankFollowerRV.layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
