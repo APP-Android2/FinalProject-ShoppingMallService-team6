201ae234f395b443ffef0009fcea19e79c1860a7
seongukHeo <heoseonguk3@gmail.com>
2024-04-11 17:49:17 +0900

    [feat] #128 UserInfoData(Model) 생성

---
+++ app/src/main/java/kr/co/lion/unipiece/model/UserInfoData.kt
@@ -0,0 +1,11 @@
+package kr.co.lion.unipiece.model
+
+data class UserInfoData (
+    var userName:String = "",
+    var nickName:String = "",
+    var phoneNumber:String = "",
+    var userId:String = "",
+    var userPwd:String = "",
+    var userIdx:Int = 0,
+    var userState:Boolean = false
+)
