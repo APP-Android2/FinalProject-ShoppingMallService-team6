f37e158145b8a25020ec45e4856ff3b3f2aaa4fe
ums1212 <voll1212@gmail.com>
2024-04-14 23:37:44 +0900

    [Feat] #132 BottomSheet에 userIdx, authorIdx 전달

--- app/src/main/java/kr/co/lion/unipiece/ui/author/AuthorInfoFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/author/AuthorInfoFragment.kt
@@ -1,7 +1,6 @@
 package kr.co.lion.unipiece.ui.author
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,8 +13,6 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kr.co.lion.unipiece.R
 import kr.co.lion.unipiece.databinding.FragmentAuthorInfoBinding
@@ -180,6 +177,10 @@ class AuthorInfoFragment : Fragment() {
         // 리뷰 버튼 클릭 시 리뷰 프래그먼트 보이기
         fragmentAuthorInfoBinding.buttonAuthorReview.setOnClickListener {
             val authorReviewBottomSheetFragment = AuthorReviewBottomSheetFragment()
+            authorReviewBottomSheetFragment.arguments = Bundle().apply {
+                putInt("userIdx", userIdx)
+                putInt("authorIdx", authorIdx)
+            }
             authorReviewBottomSheetFragment.show(parentFragmentManager, "BottomSheet")
         }
     }
