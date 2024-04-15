8c985b19e042fe12a681d558d42e3992e4795580
ums1212 <voll1212@gmail.com>
2024-04-16 01:10:46 +0900

    [Design] #154 BottomSheet 높이 조정

--- app/src/main/java/kr/co/lion/unipiece/ui/author/AuthorReviewBottomSheetFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/author/AuthorReviewBottomSheetFragment.kt
@@ -1,6 +1,8 @@
 package kr.co.lion.unipiece.ui.author
 
+import android.app.Dialog
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.google.firebase.Timestamp
@@ -124,5 +128,49 @@ class AuthorReviewBottomSheetFragment : BottomSheetDialogFragment() {
                 Toast.makeText(requireActivity(), "작성 완료", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    // 다이얼로그가 만들어질 때 자동으로 호출되는 메서드
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // 다이얼로그를 받는다.
+        val dialog = super.onCreateDialog(savedInstanceState)
+        // 다이얼로그가 보일 때 동작하는 리스너
+        dialog.setOnShowListener {
+
+            val bottomSheetDialog = it as BottomSheetDialog
+            // 높이를 설정한다.
+            setBottomSheetHeight(bottomSheetDialog)
+        }
+
+        return dialog
+    }
+
+    // BottomSheet의 높이를 설정해준다.
+    fun setBottomSheetHeight(bottomSheetDialog: BottomSheetDialog){
+        // BottomSheet의 기본 뷰 객체를 가져온다
+        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)!!
+        // BottomSheet 높이를 설정할 수 있는 객체를 생성한다.
+        val behavior = BottomSheetBehavior.from(bottomSheet)
+        // 높이를 설정한다.
+        val layoutParams = bottomSheet.layoutParams
+        layoutParams.height = getBottomSheetDialogHeight()
+        bottomSheet.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+    }
+
+    // BottomSheet의 높이를 구한다(화면 액정의 85% 크기)
+    fun getBottomSheetDialogHeight() : Int{
+        return (getWindowHeight() * 0.7).toInt()
+    }
+
+    // 사용자 단말기 액정의 길이를 구해 반환하는 메서드
+    fun getWindowHeight() : Int {
+        // 화면 크기 정보를 담을 배열 객체
+        val displayMetrics = DisplayMetrics()
+        // 액정의 가로/세로 길이 정보를 담아준다.
+        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
+        // 세로 길이를 반환해준다.
+        return displayMetrics.heightPixels
     }
 }
