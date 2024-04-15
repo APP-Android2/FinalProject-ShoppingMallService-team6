aac3e012e3d5a928f57aca61cd66b15a9abb663d
ums1212 <voll1212@gmail.com>
2024-04-15 15:16:51 +0900

    [Del] #150 필요없는 import 제거

--- app/src/main/java/kr/co/lion/unipiece/ui/mypage/ApplyVisitGalleryFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mypage/ApplyVisitGalleryFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointForward
