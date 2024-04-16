5700155140d1a69c50c4c0590f966f463d0befdd
rmadmsjm <dmswjdals12@naver.com>
2024-04-16 19:09:02 +0900

    [Feat] #155 adapter 및 recyclerView 설정

--- app/src/main/java/kr/co/lion/unipiece/ui/mygallery/adapter/SalePieceAdapter.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mygallery/adapter/SalePieceAdapter.kt
@@ -1,50 +1,47 @@
 package kr.co.lion.unipiece.ui.mygallery.adapter
 
-import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kr.co.lion.unipiece.databinding.RowSalePieceBinding
+import kr.co.lion.unipiece.model.PieceAddInfoData
 import kr.co.lion.unipiece.ui.mygallery.SalesApplicationActivity
+import kr.co.lion.unipiece.util.setImage
 
-class SalePieceAdapter (private val onItemClick: (position: Int) -> Unit): RecyclerView.Adapter<SalePieceViewHolderClass>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SalePieceViewHolderClass {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = RowSalePieceBinding.inflate(inflater, parent, false)
-        return SalePieceViewHolderClass(binding, onItemClick)
+class SalePieceAdapter (val pieceAddInfoList: List<PieceAddInfoData>, private val onItemClick: (Int) -> Unit) : RecyclerView.Adapter<SalePieceViewHolderClass>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): SalePieceViewHolderClass {
+        val binding: RowSalePieceBinding = RowSalePieceBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return SalePieceViewHolderClass(binding)
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return pieceAddInfoList.size
     }
 
     override fun onBindViewHolder(holder: SalePieceViewHolderClass, position: Int) {
-        holder.bind(holder.itemView.context, position)
+        holder.bind(pieceAddInfoList[position], onItemClick)
     }
 }
 
-class SalePieceViewHolderClass(private val binding: RowSalePieceBinding, onItemClick: (position: Int) -> Unit) : RecyclerView.ViewHolder(binding.root) {
-    init {
-        binding.root.setOnClickListener {
-            val position = adapterPosition
-            if (position != RecyclerView.NO_POSITION) {
-                onItemClick(position)
+class SalePieceViewHolderClass(val binding: RowSalePieceBinding): RecyclerView.ViewHolder(binding.root) {
+    fun bind(pieceAddInfoData: PieceAddInfoData, onItemClick: (Int) -> Unit) {
+        with(binding) {
+            root.context.setImage(imageViewSalePiece, pieceAddInfoData.addPieceImg)
+
+            textViewSalePieceState.text = pieceAddInfoData.addPieceState
+            textViewRowSalePieceName.text = pieceAddInfoData.addPieceName
+            textViewRowSalePieceArtistName.text = pieceAddInfoData.addAuthorName
+            textViewRowSalePiecePrice.text = "${pieceAddInfoData.addPiecePrice}원"
+
+            binding.buttonRowSalePieceModify.setOnClickListener {
+                val intent = Intent(root.context, SalesApplicationActivity::class.java)
+                root.context.startActivity(intent)
             }
         }
 
-        binding.root.layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-
-    }
-
-    fun bind(context: Context, position: Int) {
-        binding.textViewRowSalePieceName.text = "$position"
-        binding.buttonRowSalePieceModify.setOnClickListener {
-            val intent = Intent(context, SalesApplicationActivity::class.java)
-            context.startActivity(intent)
+        binding.root.setOnClickListener {
+            onItemClick(adapterPosition)
         }
     }
 }
