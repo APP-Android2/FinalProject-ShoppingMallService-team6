d822f66f857cbb22fa89730a27f4f0a25d9e6605
JuSungWon <79239041+language7606@users.noreply.github.com>
2024-04-17 02:23:11 +0900

    Update CartRepository.kt
    구현 덜 된 부분 제거

--- app/src/main/java/kr/co/lion/unipiece/repository/CartRepository.kt
+++ app/src/main/java/kr/co/lion/unipiece/repository/CartRepository.kt
@@ -5,8 +5,4 @@ import kr.co.lion.unipiece.model.PieceInfoData
 
 class CartRepository {
     private val cartDataSource = CartDataSource()
-
-    // 작품 번호를 통해 장바구니에 담을 작품 정보를 가져와 반환한다.
-    suspend fun getPieceDataByIdx(pieceIdx : Int):MutableList<PieceInfoData> =
-        cartDataSource.getPieceDataByIdx(pieceIdx)
 }
