7c315a41870cbe91fceeb9aabe1b149d27bb650c
JuSungWon <79239041+language7606@users.noreply.github.com>
2024-04-17 13:59:24 +0900

    Update CartRepository.kt
    머지 전 정리작업

--- app/src/main/java/kr/co/lion/unipiece/repository/CartRepository.kt
+++ app/src/main/java/kr/co/lion/unipiece/repository/CartRepository.kt
@@ -1,8 +1,5 @@
 package kr.co.lion.unipiece.repository
 
-import kr.co.lion.unipiece.db.remote.CartDataSource
-import kr.co.lion.unipiece.model.PieceInfoData
-
 class CartRepository {
-    private val cartDataSource = CartDataSource()
+    
 }
