77db69dc3184717d7f25d6febca4678e096e174d
ums1212 <voll1212@gmail.com>
2024-04-20 01:01:25 +0900

    [Feat] #205 DataSource 작성

--- app/src/main/java/kr/co/lion/unipiece/model/VisitAddData.kt
+++ app/src/main/java/kr/co/lion/unipiece/model/VisitAddData.kt
@@ -8,5 +8,16 @@ data class VisitAddData(
     var visitorNumber:String = "",
     var visitorDate: Timestamp = Timestamp.now(),
     var visitState:String = "",
-    val userIdx:Int = -1
-)
+    val userIdx:Int = -1,
+    val visitIdx:Int = -1
+){
+    // 전시실 방문 신청 수정에 사용할 Map으로 변환
+    fun instanceToMap():Map<String,Any?>{
+        return mapOf(
+            "visitorName" to visitorName,
+            "visitorPhone" to visitorPhone,
+            "visitorNumber" to visitorNumber,
+            "visitorDate" to visitorDate
+        )
+    }
+}
