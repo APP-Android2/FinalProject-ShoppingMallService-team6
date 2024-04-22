167d7814a6c304f29e2171a6ffb98db79e27e355
ums1212 <voll1212@gmail.com>
2024-04-22 10:44:17 +0900

    [Fix] #223 전시실 방문 신청 멤버 기본값 수정

--- app/src/main/java/kr/co/lion/unipiece/model/VisitAddData.kt
+++ app/src/main/java/kr/co/lion/unipiece/model/VisitAddData.kt
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat
 data class VisitAddData(
     var visitorName:String = "",
     var visitorPhone:String = "",
-    var visitorNumber:String = "",
+    var visitorNumber:String = "1",
     var visitorDate: Timestamp = Timestamp.now(),
     var visitState:String = "",
     val userIdx:Int = -1,
