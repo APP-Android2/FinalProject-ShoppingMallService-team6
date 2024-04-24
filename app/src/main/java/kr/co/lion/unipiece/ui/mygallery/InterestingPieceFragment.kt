28aa85707851f09264dcb98aee20dd430e12d94d
rmadmsjm <dmswjdals12@naver.com>
2024-04-24 13:04:00 +0900

    [Feat] #258 관심 작품 RecyclerView
    마지막 아이템 좋아요 취소 시, 해당 아이템이 화면에서 사라지지 않는 오류 해결

--- app/src/main/java/kr/co/lion/unipiece/ui/mygallery/InterestingPieceFragment.kt
+++ app/src/main/java/kr/co/lion/unipiece/ui/mygallery/InterestingPieceFragment.kt
@@ -1,11 +1,14 @@
 package kr.co.lion.unipiece.ui.mygallery
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
@@ -31,6 +34,14 @@ class InterestingPieceFragment : Fragment() {
     private val viewModel: InterestingPieceViewModel by viewModels()
 
     val userIdxPref = UniPieceApplication.prefs.getUserIdx("userIdx", 0)
+
+    private val activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_CANCELED) {
+            lifecycleScope.launch {
+                viewModel.getUserLikedPieceInfo(userIdxPref)
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentInterestingPieceBinding.inflate(inflater, container, false)
@@ -70,17 +81,25 @@ class InterestingPieceFragment : Fragment() {
                             intent.putExtra("MyGalleryFragment", true)
                             intent.putExtra("pieceIdx", likePieceInfo.pieceIdx)
                             intent.putExtra("authorIdx", likePieceInfo.authorIdx)
-                            startActivity(intent)
+                            activityResultLauncher.launch(intent)
                         },
                         onLikeClick = { position ->
                             val likePieceInfo = value[position]
-                            viewModel.cancelLikePiece(likePieceInfo.pieceIdx, userIdxPref)
+                            lifecycleScope.launch {
+                                viewModel.cancelLikePiece(likePieceInfo.pieceIdx, userIdxPref)
 
-                            createSnackBar("관심 작품에서 삭제되었습니다.", Snackbar.LENGTH_SHORT).apply {
-                                val layoutParams = this.view.layoutParams as CoordinatorLayout.LayoutParams
-                                layoutParams.setMargins(20, 0, 20, 50)
-                                this.view.layoutParams = layoutParams
-                            }.show()
+                                viewModel.likePieceInfoList.observe(viewLifecycleOwner) { likePieceList ->
+                                    if (likePieceList.isNullOrEmpty()) {
+                                        binding.recyclerViewInterestingPiece.isVisible = false
+                                    }
+                                }
+
+                                createSnackBar("관심 작품에서 삭제되었습니다.", Snackbar.LENGTH_SHORT).apply {
+                                    val layoutParams = this.view.layoutParams as CoordinatorLayout.LayoutParams
+                                    layoutParams.setMargins(20, 0, 20, 50)
+                                    this.view.layoutParams = layoutParams
+                                }.show()
+                            }
                         })
 
                     with(binding) {
